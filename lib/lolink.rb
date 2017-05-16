b8d5f42401b1c499c91ff611b3baafe9ffcce1e3
kikyous <kikyous@163.com>
2017-05-16 16:18:04 +0800

    version 0.0.7

--- lib/lolink.rb
+++ lib/lolink.rb
@@ -4,9 +4,18 @@ require 'nokogiri'
 require 'lolink/version'
 
 class Lolink
-  def initialize
+  attr_accessor :versions
+  def initialize(v=nil)
     @base_url = "http://down.qq.com/lol"
-    parse_version
+    if v
+      self.versions = v
+    else
+      parse_version
+    end
+  end
+
+  def version
+    @version ||= @versions.split('-').last
   end
 
   def parse_version
@@ -15,8 +24,6 @@ class Lolink
     link = @doc.search('.downlst.fl a#xzq').first.attr('href')
 
     @versions = link.match(/(V.+-V.+)_PATCH/)[1]
-
-    @version = @versions.split('-').last
   end
 
   def test_link
@@ -34,19 +41,23 @@ class Lolink
     links
   end
 
+  def version_number
+    version.gsub /[^\d]/, ''
+  end
+
   def full_links
-    files = ["http://dlied1.qq.com/lol/full/LOL_#{@version}_FULL.exe"]
+    files = ["http://dlied1.qq.com/lol/full/#{version_number}/LOL_#{version}_FULL.exe"]
 
     files += test_link do |i|
-      "http://dlied1.qq.com/lol/full/LOL_#{@version}_FULL.7z.#{i}"
+      "http://dlied1.qq.com/lol/full/#{version_number}/LOL_#{version}_FULL.7z.#{i}"
     end
   end
 
   def patch_links
-    files = ["#{@base_url}/patch/LOL_#{@versions}_PATCH.exe"]
+    files = ["#{@base_url}/patch/#{version_number}/LOL_#{versions}_PATCH.exe"]
 
     files += test_link do |i|
-      "#{@base_url}/patch/LOL_#{@version}_PATCH.7z.#{i}"
+      "#{@base_url}/patch/#{version_number}/LOL_#{version}_PATCH.7z.#{i}"
     end
   end
 
--- lib/lolink/version.rb
+++ lib/lolink/version.rb
@@ -1,3 +1,3 @@
 class Lolink
-  Version = '0.0.6'
+  Version = '0.0.7'
 end
